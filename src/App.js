dc3404e143b70a6548649ad3d3ecaeea13c4d8fe
pohern <ohern_p@yahoo.com>
2021-10-07 11:04:13 -0500

    Awesome react table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { AgGridColumn, AgGridReact } from "ag-grid-react";
 
-function App() {
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+
+const App = () => {
+  // const rowData = [
+  //   { make: "Toyota", model: "Celica", price: 35000 },
+  //   { make: "Ford", model: "Mondeo", price: 32000 },
+  //   { make: "Porsche", model: "Boxter", price: 72000 },
+  // ];
+  const [rowData, setRowData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://www.ag-grid.com/example-assets/row-data.json")
+      .then((result) => result.json())
+      .then((rowData) => setRowData(rowData));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+      <AgGridReact rowData={rowData}>
+        <AgGridColumn field="make" sortable={true} filter={true}></AgGridColumn>
+        <AgGridColumn
+          field="model"
+          sortable={true}
+          filter={true}
+        ></AgGridColumn>
+        <AgGridColumn
+          field="price"
+          sortable={true}
+          filter={true}
+        ></AgGridColumn>
+      </AgGridReact>
     </div>
   );
-}
+};
 
 export default App;
